4e55ba00e95c6ef061cbe7120e79b6c8a2f3bb91
pfabricio555 <pfabricio555@gmail.com>
2021-02-01 22:26:03 -0300

    Gerando primeiro relatório de orçamentos

--- rel/rel_orcamento_class.php
+++ rel/rel_orcamento_class.php
@@ -4,4 +4,25 @@
 require_once '../dompdf/autoload.inc.php';
 use Dompdf\Dompdf;
 
+// ALIMENTAR OS DADOS NO RELATÓRIO
+$html = "Este é o conteúdo do relatório";
+
+//INICIAZIAR A CLASSE DO DOMPDF
+$pdf = new DOMPDF();
+
+// DEFINIR O TAMANHO DO PAPEL E ORIENTAÇÃO DA PÁGINA
+$pdf->set_paper(array(0,0,210, 297));
+
+//CARREGAR O CONTEÚDO HTML
+$pdf->load_html(utf8_decode($html));
+
+//RENDERIZAR O PDF
+$pdf->render();
+
+//NOMEAR O PDF GERADO
+$pdf->stream(
+'relatorioOrcamento.pdf',
+array("Attachment => false");
+);
+
 ?>
