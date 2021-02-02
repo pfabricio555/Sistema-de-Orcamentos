885845e1910afd33c7aa0ef9b4d7aecb070427ac
pfabricio555 <pfabricio555@gmail.com>
2021-02-02 20:27:07 -0300

    Estilizando Pdf de orçamentos

--- rel/rel_orcamento.php
+++ rel/rel_orcamento.php
@@ -1,26 +1,84 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 <style>
 
+ @page {
+            margin: 0px;
 
-@page {
-    margin-top: 0px;
-    margin-left: 0px;
-    margin-right:0px;
+        }
+
+.footer {
+    position:absolute;
+    bottom:0;
+    width:100%;
+    background-color: #ebebeb;
+    padding:10px;
 }
 
-.footer{
-    position: fixed;
-    bottom:0;
-    left:0;
+.cabecalho {    
+    background-color: #ebebeb;
+    padding-top:15px;
+    margin-bottom:15px;
+}
+
+.titulo{
+	margin:0;
 }
 
 </style>
 
-<div class="alert alert-secondary" role="alert">
-    <img src="http://localhost/Sistema-de-Orcamentos/img/Logo.png"/> <!-- O caminho da imagem tem que ser absoluto!-->
+<div class="cabecalho">
+	<div class="row container">
+		<div class="col-sm-6">	
+            <img src="http://localhost/Sistema-de-Orcamentos/img/Logo.png" width="250px"/> <!-- O caminho da imagem tem que ser absoluto!-->
+		</div>
+		<div class="col-sm-6">	
+			<h3 class="titulo"><b>JC Inject - Modúlos de Injeção</b></h3>
+			<h6 class="titulo">Rua da JC Inject Nº 1000, Zona Sul - SP - SP - CEP 00000-000</h6>
+		</div>
+	</div>
 </div>
 
-<div class="footer alert alert-secondary" role="alert">
-<footer class="navbar-fixed-bottom">RODAPÉ</footer>
+<div class="container">
+
+    <div class="row">
+        <div class="col-sm-8">
+            <big> Orçamento Nº 50</big>
+        </div>
+        <div class="col-sm-4">
+            <big> Data: 10/10/2021 </big>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="col-sm-12">
+            <p style="font-size:12px"><b>Dados do Cliente</b></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <p style="font-size:12px"><b>Nome:</b></p>
+        </div>
+        <div class="col-sm-3">
+            <p style="font-size:12px"><b>Email:</b></p>
+        </div>
+        <div class="col-sm-3">
+            <p style="font-size:12px"><b>Endereço:</b></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <p style="font-size:12px"><b>Telefone:</b></p>
+        </div>
+        <div class="col-sm-3">
+            <p style="font-size:12px"><b>CPF:</b></p>
+        </div>
+    </div>
+
+</div>
+
+<div class="footer">
+RODAPÉ
 </div>
