c0c3dda50eb56800f0913a0c3164846743ce3b16
pfabricio555 <pfabricio555@gmail.com>
2021-02-02 22:39:11 -0300

    Prazo para fechar o orçamento

--- rel/rel_orcamento.php
+++ rel/rel_orcamento.php
@@ -3,11 +3,13 @@ include('../conexao.php');
 
 $id = $_GET['id'];
 
-$query = "select * from orcamentos where id = '$id'";
+$query = "select o.id, o.cliente, o.tecnico, o.produto, o.serie, o.problema, o.laudo, o.valor_servico, o.pecas, o.valor_pecas, o.total, o.valor_total, o.status, o.data_abertura, o.data_geracao, c.nome as cli_nome, c.email, c.telefone, c.endereco, f.nome as func_nome from orcamentos as o INNER JOIN clientes as c on o.cliente = c.cpf INNER JOIN funcionarios as f on o.tecnico = f.id where o.id = '$id'";
 $result = mysqli_query($conexao, $query);
 
 while($res_1 = mysqli_fetch_array($result)){
 
+$data = $res_1["data_geracao"];
+$data2 = implode('/', array_reverse(explode('-', $data)));
 ?>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -78,7 +80,7 @@ while($res_1 = mysqli_fetch_array($result)){
             <big> Orçamento Nº <?php echo $id ?></big>
         </div>
         <div class="col-sm-4">
-            <big> Data: 10/10/2021 </big>
+            <big> Data: <?php echo $data2;?> </big>
         </div>
     </div>
 
@@ -91,18 +93,18 @@ while($res_1 = mysqli_fetch_array($result)){
     </div>
     <div class="row">
         <div class="col-sm-3">
-            <p style="font-size:12px">Nome:</p>
+            <p style="font-size:12px">Nome: <?php echo $res_1['cli_nome'];?></p>
         </div>
         <div class="col-sm-3">
-            <p style="font-size:12px">Email:</p>
+            <p style="font-size:12px">Email: <?php echo $res_1['email'];?></p>
         </div>
         <div class="col-sm-3">
-            <p style="font-size:12px">Endereço:</p>
+            <p style="font-size:12px">Endereço: <?php echo $res_1['endereco'];?></p>
         </div>
     </div>
     <div class="row">
         <div class="col-sm-3">
-            <p style="font-size:12px">Telefone:</p>
+            <p style="font-size:12px">Telefone: <?php echo $res_1['telefone'];?></p>
         </div>
         <div class="col-sm-3">
             <p style="font-size:12px">CPF: <?php echo $res_1['cliente']; ?></p>
@@ -155,8 +157,8 @@ while($res_1 = mysqli_fetch_array($result)){
             <td> <b>Quantidade<b> </td>
         </tr>
         <tr>
-            <td> Peça </td>
-            <td> Valor Peça </td>
+            <td> <?php echo $res_1['pecas'];?> </td>
+            <td> R$ <?php echo $res_1['valor_pecas'];?> </td>
             <td> 1 </td>
         </tr>
     </table>
@@ -169,17 +171,26 @@ while($res_1 = mysqli_fetch_array($result)){
             
         </div>
         <div class="col-sm-4 areaTotais">
-            <p class="pgto" style="font-size:14px"><b>Total de peças:</b></p>
-            <p class="pgto" style="font-size:14px"><b>Total Mão de Obra:</b></p>
+            <p class="pgto" style="font-size:14px"><b>Total de peças:</b> R$ <?php echo $res_1['valor_pecas'];?></p>
+            <p class="pgto" style="font-size:14px"><b>Total Mão de Obra:</b> R$ <?php echo $res_1['valor_servico'];?></p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-sm-6">
-            <p style="font-size:14px"><b>Técnico:</b></p>
+            <p style="font-size:14px"><b>Técnico:</b> <?php echo $res_1['func_nome'];?></p>
         </div>
         <div class="col-sm-4 areaTotal">
-            <p style="font-size:14px"><b>Total a pagar:</b></p>
+            <p style="font-size:14px"><b>Total a pagar:</b> R$ <?php echo $res_1['valor_total'];?></p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            
+        </div>
+        <div class="col-sm-3">
+            <p style="font-size:10px">Orçamento válido até:  <?php echo date('d/m/Y', strtotime("+5 days",strtotime($res_1['data_geracao'])));?></p>
         </div>
     </div>
     
